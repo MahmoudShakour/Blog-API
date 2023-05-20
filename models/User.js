cd48aea3e3666a0b3448997fe27c0e7c8a4186c5
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-05-20 07:56:20 +0300

    Create user Schema

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstname: { type: String },
+  lastname: { type: String },
+  email: { type: String },
+  password: { type: String },
+  username: { type: String },
+  role: { type: String },
+});
+
+userSchema.virtual("url").get(function () {
+  return "/user/" + this._id;
+});
+
+module.exports = mongoose.model("User", userSchema);
